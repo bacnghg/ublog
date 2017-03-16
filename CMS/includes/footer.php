900b13b04db958b6952ad851f89cfa3f22633fa2
Bob Nguyen <nhb.fit.hut@gmail.com>
2017-03-16 11:00:26 +0700

    First commit

---
+++ CMS/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="navbar navbar-default navbar-fixed-bottom">
+		<div class="container">
+			<span class="navbar-text pull-left">Copyright © 2016 KingBob. All rights reserved.</span>
+			
+				<a class="btn btn-primary pull-right navbar-btn" href="#">Share</a>
+			
+		</div>
+</footer>
